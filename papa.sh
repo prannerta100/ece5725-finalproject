0bbf659d04a4f6a0bd9b24ce546e84c6bb1839bf
prannerta100 <prannerta100@github.com>
2020-12-07 08:50:19 -0500

    some code; Pranav

---
+++ papa.sh
@@ -0,0 +1,11 @@
+video_id="001"
+input_mp4file="001_ManUdhanMarathi.mp4"
+output_wavfile="Video${video_id}_$(date "+%Y.%m.%d-%H.%M.%S").wav"; 
+rm -rf $output_wavfile
+rm -rf video_fifo #remove the fifo if it exists
+mkfifo video_fifo #create a new fifo
+videolen=`ffprobe -v error -show_entries format=duration -of default=noprint_wrappers=1:nokey=1 $input_mp4file`
+sudo SDL_VIDEODRIVER=fbcon SDL_FBDEV=/dev/fb1 mplayer -input file=video_fifo -vo sdl -framedrop $input_mp4file  &
+arecord -D plughw:2,0 -d $videolen $output_wavfile
+#Twinkle.mp4 #bigbuckbunny320p.mp4
+#mplayer -ao alsa:device=hw=1.0 Twinkle.mp4
