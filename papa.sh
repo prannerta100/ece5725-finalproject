db83a04d83aea3d65448302dffe1254147c254bb
prannerta100 <prannerta100@github.com>
2020-12-07 12:12:01 -0500

    fix: update file format to accommodate higher amplitude

--- papa.sh
+++ papa.sh
@@ -5,7 +5,7 @@ rm -rf $output_wavfile
 rm -rf video_fifo #remove the fifo if it exists
 mkfifo video_fifo #create a new fifo
 videolen=`ffprobe -v error -show_entries format=duration -of default=noprint_wrappers=1:nokey=1 $input_mp4file`
-sudo SDL_VIDEODRIVER=fbcon SDL_FBDEV=/dev/fb1 mplayer -input file=video_fifo -vo sdl -framedrop $input_mp4file  &
-arecord -D plughw:2,0 -d $videolen $output_wavfile
+sudo SDL_VIDEODRIVER=fbcon SDL_FBDEV=/dev/fb1 mplayer -input file=video_fifo -vo sdl -ao alsa:device=hw=1.0 -framedrop $input_mp4file  &
+arecord -D plughw:2,0 -f S32_LE -d `printf "%.0f" $(echo scale=3;$videolen | bc)` user_recordings/$output_wavfile
 #Twinkle.mp4 #bigbuckbunny320p.mp4
 #mplayer -ao alsa:device=hw=1.0 Twinkle.mp4
