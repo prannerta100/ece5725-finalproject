1f5e770aa3e817d3f7ba95dc00f500b3fa24814f
prannerta100 <prannerta100@github.com>
2020-12-17 11:01:25 -0500

    scoring done

--- papa.sh
+++ papa.sh
@@ -1,11 +1,28 @@
+#video id
 video_id="001"
+
+#input mp4 file from video/ folder
 input_mp4file="video/001_ManUdhanMarathi.mp4"
+
+#output file with time stamp; this will contain the user recording .wav
 output_wavfile="Video${video_id}_$(date "+%Y.%m.%d-%H.%M.%S").wav"; 
-rm -rf $output_wavfile
+
+#remove video_fifo if exists 
 rm -rf video_fifo #remove the fifo if it exists
-mkfifo video_fifo #create a new fifo
+
+#create a new fifo, for emergency control of the mplayer 
+#(not needed, unless you try to stop the karaoke song midway)
+mkfifo video_fifo 
+
+#calculate length of the video
 videolen=`ffprobe -v error -show_entries format=duration -of default=noprint_wrappers=1:nokey=1 $input_mp4file`
+
+#run the lyrical video through mplayer 
 sudo SDL_VIDEODRIVER=fbcon SDL_FBDEV=/dev/fb1 mplayer -input file=video_fifo -vo sdl -ao alsa:device=hw=1.0 -framedrop $input_mp4file  &
+
+#record the user song
 arecord -D plughw:2,0 -f S32_LE -d `printf "%.0f" $(echo scale=3;$videolen | bc)` user_recordings/$output_wavfile
+
+#misc, ignore!
 #Twinkle.mp4 #bigbuckbunny320p.mp4
 #mplayer -ao alsa:device=hw=1.0 Twinkle.mp4
---
+++ score.py
@@ -0,0 +1,42 @@
+import sys, re, 
+import soundfile as sf
+import pandas as pd
+import numpy as np
+
+#min 3 arguments needed
+if len(sys.argv) < 3:
+    sys.exit()
+else:
+    src_file = 'user_recordings/' + sys.argv[1]
+    ref_file = re.sub('\.mp4', '.wav', sys.argv[2])
+
+print('Input file: 'src_file, ' Output File: ', ref_file)
+
+#read the source and reference files
+data_src, samplerate_src = sf.read(src_file)
+data_ref, samplerate_ref = sf.read(ref_file)
+
+#choose the first channel from the reference audio, discard the rest
+data_ref = data_ref[:,0]
+
+#define pandas dataframes with the appropriate time index
+data_ref_df = pd.DataFrame({'ref':data_ref}, index = np.arange(len(data_ref)) / samplerate_ref)
+data_src_df = pd.DataFrame({'src':data_src}, index = np.arange(len(data_src)) / samplerate_src)
+
+#"outer" join the 2 data frames and then interpolate to fill in the gaps
+df = data_ref_df.join(data_src_df, how='outer').apply(pd.Series.interpolate)
+
+#find the max run times of each video, take the minimum
+#we calculate the score based on the minimum of the song time and the user's recording duration
+#nominally these times should be the same
+run_time = min(max(data_ref_df.index), max(data_src_df.index))
+df = df.loc[df.index <= run_time]
+
+#correlation score
+correlation = 50 * min(np.abs(df.corr().iloc[0,1]), 0.03) / 0.03
+#persistence score
+persistence = 50 * df.loc[np.abs(df['src']) > 0.1 * np.max(np.abs(df['src']))].shape[0] / \
+                   df.loc[np.abs(df['ref']) > 0.1 * np.max(np.abs(df['ref']))].shape[0]
+#sum the 2 scores (total is out of 100)
+score = int(round(correlation + persistence))
+print(score)
